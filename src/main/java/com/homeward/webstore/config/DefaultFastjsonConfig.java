ba34493c56613b746646a73de198b4d76dbd6174
Caishangqi <2470159730@qq.com>
2022-02-10 02:38:46 -0800

    默认json配置

--- src/main/java/com/homeward/webstore/config/DefaultFastjsonConfig.java
+++ src/main/java/com/homeward/webstore/config/DefaultFastjsonConfig.java
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.Configuration;
 
 import java.math.BigInteger;
 import java.nio.charset.Charset;
-//是
+//是s
 @Configuration
 public class DefaultFastjsonConfig {
 
