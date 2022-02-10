daaa92209de3234ca563b362c48a36f700387b83
Ba1oretto <sunjiamu@outlook.com>
2022-02-11 07:51:56 +0900

    定义拦截器

--- src/main/java/com/homeward/webstore/aop/LoginInterceptor.java
+++ src/main/java/com/homeward/webstore/aop/LoginInterceptor.java
@@ -1,13 +1,24 @@
 package com.homeward.webstore.aop;
 
+import com.homeward.webstore.mapper.PlayerInfoMapper;
+import com.homeward.webstore.pojo.playerinfo.PlayerInfo;
+import com.homeward.webstore.util.sysconst.Const;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-//@Component
+@Component
 public class LoginInterceptor implements HandlerInterceptor {
+    private final PlayerInfoMapper playerInfoMapper;
+
+    public LoginInterceptor(PlayerInfoMapper playerInfoMapper) {
+        this.playerInfoMapper = playerInfoMapper;
+    }
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         //获取cookie
@@ -19,21 +30,34 @@ public class LoginInterceptor implements HandlerInterceptor {
         }
 
         //获取cookie中的用户信息
-        String cookie_name = null;
+        String cookie_value = null;
         for (Cookie cookie : cookieArrays) {
             if ("cookie_username".equals(cookie.getName())) {
-                cookie_name = cookie.getValue();
+                cookie_value = cookie.getValue();
                 break;
             }
         }
 
         //二次验证cookie数据
-        if (StringUtils.isEmpty(cookie_name)) {
+        if (StringUtils.isEmpty(cookie_value)) {
             response.sendRedirect(request.getContextPath() + "/RedirectPage.html");
             return false;
         }
 
+        //获取session
+        HttpSession session = request.getSession();
 
-        return HandlerInterceptor.super.preHandle(request, response, handler);
+        //获取session中的用户信息
+        Object sessionAttribute = session.getAttribute(Const.SYSTEM_USER_SESSION);
+
+        //判断是否过期
+        if (sessionAttribute == null) {
+            PlayerInfo playerInfo = playerInfoMapper.getPlayerInfo(cookie_value);
+
+            //将信息保存到session
+            session.setAttribute("cookie_username", playerInfo);
+        }
+
+        return true;
     }
 }
--- src/main/java/com/homeward/webstore/config/InterceptorConfig.java
+++ src/main/java/com/homeward/webstore/config/InterceptorConfig.java
@@ -2,32 +2,34 @@ package com.homeward.webstore.config;
 
 import com.homeward.webstore.aop.LoginInterceptor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.ArrayList;
 import java.util.List;
 
-//@Configuration
+@Configuration
+@PropertySource("classpath:/interceptor.yml")
 public class InterceptorConfig implements WebMvcConfigurer {
-    private final LoginInterceptor loginInterceptor;
 
-
-    @Value("${interceptor.addPath:/*}")
-    private List<String> addPath;
-
-    @Value("${interceptor.excludePath:login}")
+//    @Value("interceptor.includePath:")
+//    private List<String> includePath;
+    @Value("interceptor.excludePath:/name/*")
     private List<String> excludePath;
 
-
+    private final LoginInterceptor loginInterceptor;
     public InterceptorConfig(LoginInterceptor loginInterceptor) {
         this.loginInterceptor = loginInterceptor;
     }
 
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        InterceptorRegistration interceptorRegistration = registry.addInterceptor(loginInterceptor);
-        interceptorRegistration.addPathPatterns(addPath);
-        interceptorRegistration.excludePathPatterns(excludePath);
+//        InterceptorRegistration interceptorRegistration = registry.addInterceptor(loginInterceptor);
+////        interceptorRegistration.addPathPatterns(includePath);
+//        interceptorRegistration.excludePathPatterns(excludePath);
     }
 }
