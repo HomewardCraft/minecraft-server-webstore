5c1c805f7e175ba9d695cb30cab9cf37d2884079
Ba1oretto <sunjiamu@outlook.com>
2022-02-11 07:52:35 +0900

    常量类

---
+++ src/main/java/com/homeward/webstore/util/sysconst/Const.java
@@ -0,0 +1,27 @@
+package com.homeward.webstore.util.sysconst;
+
+public class Const {
+
+    /**
+     * 项目名称
+     */
+    public static final String SYSTEM_ITEM_NAME = "homeward-store";
+
+    /**
+     * 登录用户session
+     */
+    public static final String SYSTEM_USER_SESSION = "systemUserSession";
+
+    /**
+     * 用户权限菜单
+     */
+    public static final String SYSTEM_USER_MENU = "system_user_menu";
+    /**
+     * 用户角色
+     */
+    public static final String SYSTEM_USER_ROLE = "system_user_role";
+    /**
+     * 用户权限
+     */
+    public static final String SYSTEM_USER_PERMISSION = "system_user_permission";
+}
