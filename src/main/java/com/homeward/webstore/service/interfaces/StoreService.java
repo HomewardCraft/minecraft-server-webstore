388011fe8abcbd13647960056311ce59bd89795c
Ba1oretto <sunjiamu@outlook.com>
2022-02-10 23:52:09 +0900

    移除文件

--- src/main/java/com/homeward/webstore/service/interfaces/StoreService.java
+++
@@ -1,9 +0,0 @@
-package com.homeward.webstore.service.interfaces;
-
-import com.homeward.webstore.pojo.store.Items;
-
-import java.util.List;
-
-public interface StoreService {
-    List<Items> getSpecificItems(String type);
-}
