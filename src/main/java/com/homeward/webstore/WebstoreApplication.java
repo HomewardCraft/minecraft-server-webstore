34ee50fe6a829e54123d810ff217a5b758d87f72
Ba1oretto <sunjiamu@outlook.com>
2022-02-12 00:10:07 +0900

    完善配置类结构

--- src/main/java/com/homeward/webstore/WebstoreApplication.java
+++ src/main/java/com/homeward/webstore/WebstoreApplication.java
@@ -1,27 +1,13 @@
 package com.homeward.webstore;
 
-import com.homeward.webstore.util.JsonMap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class WebstoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebstoreApplication.class, args);
-    }
-
-    //注册rest template
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
-
-    //注册自己的json map工具类
-    @Bean
-    public JsonMap jsonMap() {
-        return new JsonMap();
     }
 }
