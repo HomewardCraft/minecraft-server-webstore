2782eb715f7195c448c8b40fdf123e8e5a976160
Caishangqi <Caiziii@126.com>
2022-02-14 16:18:06 -0800

    Vue 配置文件

---
+++ src/main/resources/static/webstore/vue.config.js
@@ -0,0 +1,15 @@
+//项目配置
+module.exports = {
+
+    // pages: {
+    //
+    //     index: {
+    //         //入口
+    //         // entry: 'src/index/main.js'
+    //     }
+    //
+    // },
+
+    lintOnSave : false //关闭语法检查
+
+}
