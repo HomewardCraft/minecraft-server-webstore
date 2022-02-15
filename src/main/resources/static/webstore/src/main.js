58a87c2749f071992fc2704691f724902885e81c
Caishangqi <Caiziii@126.com>
2022-02-14 17:10:31 -0800

    Caizii 编写的弱智开发者插件

--- src/main/resources/static/webstore/src/main.js
+++ src/main/resources/static/webstore/src/main.js
@@ -1,8 +1,18 @@
+//引入Vue
 import Vue from 'vue'
+//引入app组件
 import App from './App.vue'
-
+//引入插件
+import developerplugin from "./components/plugins/developer";
+//使用插件
+Vue.use(developerplugin)
+//关闭vue生产提示
 Vue.config.productionTip = false
 
+//创建vm
 new Vue({
+  el: '#app',
+  //模板渲染器
   render: h => h(App),
-}).$mount('#app')
+})
+    // .$mount('#app')
