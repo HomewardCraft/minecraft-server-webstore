9c61c9801e5a0c6546cb29a161fed294977ebf28
Ba1oretto <sunjiamu@outlook.com>
2022-02-17 20:06:17 +0900

    日志

--- src/main/java/com/homeward/webstore/config/ViewConfig.java
+++ src/main/java/com/homeward/webstore/config/ViewConfig.java
@@ -24,6 +24,7 @@ public class ViewConfig implements WebMvcConfigurer {
                 "/**"
         );
         loginInterceptor.excludePathPatterns(
+                "/category/*",
                 "/name/*"
         );
     }
