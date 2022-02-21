00962ae2e371dfab18bd020198619bca6b17b3b5
Caishangqi <Caiziii@126.com>
2022-02-21 04:15:26 -0800

    1

---
+++ src/webstore/src/components/buttom/brules.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="bt">
+    RULES
+  </div>
+</template>
+
+<script>
+export default {
+  name: "brules"
+}
+</script>
+
+<style scoped>
+div{
+  width: 100px;
+  height: 40px;
+  display: block;
+  text-align: center;
+  margin-right: 15px;
+  margin-left: 15px;
+  border: 1px solid transparent;
+}
+.bt:hover{
+
+  background-color: #06a90d;
+  border-style:solid;
+  border-color: #94e199;
+  border-width:1px;
+}
+</style>
---
+++ src/webstore/src/components/wheader.vue
@@ -0,0 +1,43 @@
+<template>
+
+  <div class="grid">
+    <div class="span-row-3"><wlogo></wlogo></div>
+    <div><wrouter></wrouter></div>
+    <div class="span-row-2"><wheaderinfo></wheaderinfo></div>
+  </div>
+
+
+</template>
+
+<script>
+
+import wrouter from "./wrouter";
+import wheaderinfo from "./wheaderinfo";
+import wlogo from "./wlogo";
+
+
+export default {
+  name: "wheader",
+  components: {wrouter, wheaderinfo, wlogo}
+}
+</script>
+
+<style scoped>
+
+.grid {
+  margin-top: 10px;
+  margin-bottom: 80px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+}
+.span-row-3{grid-row: span 3 / auto;}
+
+.span-row-2{grid-row: span 2 / auto;}
+
+div{
+  /*margin: 0;*/
+  /*width: 1100px;*/
+  /*height: 100px;*/
+  /*display: block;*/
+}
+</style>
