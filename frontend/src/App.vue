73d62ce19b59c8a824653be8e16783b69da31da4
Ba1oretto <sunjiamu@outlook.com>
2022-02-22 11:05:07 +0900

    1

---
+++ frontend/src/App.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="app">
+    <br>
+    <wheader></wheader>
+    <wsale class="wsale"></wsale>
+    <br>
+    <wcount></wcount>
+    <span class="cat">
+      Categories
+    </span>
+    <wcategories></wcategories>
+    <whelp></whelp>
+    <wfooter></wfooter>
+  </div>
+</template>
+
+<script>
+//导入组件
+import HelloWorld from './components/HelloWorld.vue'
+import wheader from "./components/wheader";
+import wsale from "./components/wsale";
+import wcount from "./components/wcount";
+import wcategories from "./components/wcategories";
+import whelp from "./components/whelp";
+import wfooter from "./components/wfooter";
+import Wrouter from "./components/wrouter";
+import Wcrates from "./components/wcrates";
+
+
+
+export default {
+  name: 'App',
+  components: {
+    Wcrates,
+    Wrouter,
+    HelloWorld,
+    wheader,
+    wsale,
+    wcount,
+    wcategories,
+    whelp,
+    wfooter
+  }
+}
+</script>
+
+<style>
+
+wcategories{
+
+}
+
+div{
+
+}
+
+.cat {
+  padding: 20px 0;
+  margin:auto;
+  font-weight: bold;
+  font-size: 28px;
+  color: #FFFFFF;
+  width: 1100px;
+  height: 30px;
+  display: block;
+  text-align: left;
+}
+
+
+.wsale {
+  alignment: center;
+}
+
+#app {
+  width:100%;
+  background-color: black;
+  margin: 0 auto;
+  overflow:hidden;
+  position:relative;
+}
+</style>
---
+++ frontend/src/components/wranks.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="bg" :class="bgvue" @mouseover="dark()" @mouseout="origin()">
+    <span>
+      Ranks
+    </span>
+    <div class="img" :class="hover">
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "wranks",
+  data() {
+    return {
+      hover: '',
+      bgvue: ''
+    }
+
+  },
+  methods: {
+    dark() {
+      this.hover = 'hover'
+      this.bgvue = 'turndark'
+    },
+    origin() {
+      this.hover = ''
+      this.bgvue = ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+div{
+  margin:10px;
+}
+
+.turndark{
+  filter:brightness(0.8);
+}
+
+span {
+  margin: 0;
+  text-align: left;
+  padding: 30px;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+  color: #FFFFFF;
+}
+
+.img {
+  margin-left: 20px;
+  margin-top:15px;
+  background-image: url(../assets/ranks.png);
+  background-repeat: no-repeat;
+  background-size: 35%;
+  width: 100%;
+  height: 94%;
+  display: block;
+}
+
+.bg {
+  border: 2px solid #9d66ac;
+  background-color: #650f7e;
+  background-size: 100%;
+  width: 580px;
+  height: 240px;
+  display: block;
+  text-align: center;
+  margin-right: 15px;
+  margin-left: 15px;
+}
+
+/*过期
+/*.img:hover {*/
+/*  background-image: url(../assets/crates_hover.png);*/
+/*}*/
+
+.hover {
+  background-image: url(../assets/ranks_hover.png);
+
+}
+
+
+
+</style>
