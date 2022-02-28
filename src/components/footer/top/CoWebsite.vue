0c97b600b566c92a632966975d76873e0dad691d
Ba1oretto <sunjiamu@outlook.com>
2022-02-28 21:21:10 +0900

    实现点击复制

--- src/components/footer/top/CoWebsite.vue
+++ src/components/footer/top/CoWebsite.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="uppercase font-bold text-custom-1000 text-sm justify-center cursor-pointer flex relative lg:items-stretch transition-opacity duration-150 ease-in-out hover:opacity-80">
-    <div class="bg-custom-900 tracking-widest py-2 px-3" @click="copyIpAddress">kkk.fantang.cc</div>
+    <div class="tooltips bg-custom-900 tracking-widest py-2 px-3" @click="copyIpAddress" data-clipboard-action="copy" :data-clipboard-text="'kkk.fantang.cc'">kkk.fantang.cc</div>
     <div class="flex items-center px-3 tracking-wide bg-custom-900 opacity-75">236</div>
     <div class="absolute mb-2 left-0.5 bottom-0 translate-y-2.5 text-xs font-bold uppercase transition-opacity ease-in-out duration-200 text-custom-700" :class="opacity">ip copied</div>
   </div>
@@ -14,14 +14,16 @@ export default {
 
 <script setup>
 import {ref} from "vue";
+import ClipboardJS from "clipboard";
 
 let timer
 let opacity = ref('opacity-0')
 
 function copyIpAddress() {
-  let clipboard = new Clipboard()
-
-  clipboard.
+  let clipboard = new ClipboardJS('.tooltips')
+  clipboard.on('success', () => {
+    clipboard.destroy();
+  })
 
   clearTimeout(timer)
   opacity.value = 'opacity-1'
