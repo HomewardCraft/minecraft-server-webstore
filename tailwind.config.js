622f2719139ac0ad26c78731e3561c0814fb2256
Ba1oretto <sunjiamu@outlook.com>
2022-02-28 20:52:09 +0900

    在tailwind中配置基础的样式

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,38 @@ module.exports = {
   content: [
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
+  darkMode: 'class',
+  // 添加tailwind的选择器的字首
+  // prefix: 'tw-',
+
+  // 主题配置
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    // 响应式断点
+    // screens: {
+    //   sm: '480px',
+    //   md: '768px',
+    //   lg: '976px',
+    //   xl: '1440px',
+    // },
+
+    // 继承默认配置
+    extend: {
+      // 拓展后缀
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+        '110': '27.5rem'
+      },
+      gridTemplateRows: {
+        'body': 'auto 1fr auto'
+      },
+      colors: {
+        custom: {
+          700: '#f4b174',
+          900: '#dc8a16',
+          1000: '#a35710'
+        }
+      }
+    }
+  }
 }
