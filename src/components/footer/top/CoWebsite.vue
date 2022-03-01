9be3a56d35bfb84e110614c6bfac618d57759788
Ba1oretto <sunjiamu@outlook.com>
2022-03-02 05:04:41 +0900

    主界面大部分内容完成

--- src/components/footer/top/CoWebsite.vue
+++ src/components/footer/top/CoWebsite.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="uppercase font-bold text-custom-1000 text-sm justify-center cursor-pointer flex relative lg:items-stretch transition-opacity duration-150 ease-in-out hover:opacity-80">
     <div class="tooltips bg-custom-900 tracking-widest py-2 px-3" @click="copyIpAddress" data-clipboard-action="copy" :data-clipboard-text="'kkk.fantang.cc'">kkk.fantang.cc</div>
-    <div class="flex items-center px-3 tracking-wide bg-custom-900 opacity-75">236</div>
+    <div class="flex items-center px-3 tracking-wide bg-custom-900 opacity-75">1919</div>
     <div class="absolute mb-2 left-0.5 bottom-0 translate-y-2.5 text-xs font-bold uppercase transition-opacity ease-in-out duration-200 text-custom-700" :class="opacity">ip copied</div>
   </div>
 </template>
