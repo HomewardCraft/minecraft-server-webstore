16be13642481b028b365b8d940a709efda0eda75
Ba1oretto <sunjiamu@outlook.com>
2022-03-02 02:04:53 +0900

    安装路由

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import router from "./router/index.js";
+
 import './index.css'
 
 const app = createApp(App)
+
+app.use(router)
 
 app.mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,10 @@
+import homepageRoute from "../pages/HomepageRoute.vue";
+
+const routes = [
+    { path: '/', component: homepageRoute }
+]
+
+export default VueRouter.createRouter({
+    history: VueRouter.createWebHashHistory(),
+    routes
+})
