3cec36905e5459adebe115e609318c97ddd470ab
Ba1oretto <sunjiamu@outlook.com>
2022-03-03 02:15:21 +0900

    配置代理

--- src/components/modal/LoginInput.vue
+++ src/components/modal/LoginInput.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="form p-10 lg:col-span-2">
-    <form>
-      <input
-          class="bg-gray-800 text-white block py-2 px-4 w-full mb-5 border border-light text-center transition-colors duration-150 ease-in-out focus:border-yellow-400 focus:outline-none"
-          autocomplete="off" pattern="^[A-Za-z0-9_]{1,16}$" spellcheck="false" maxlength="16" v-model = "username"/>
-      <button
-          class="bg-btn border border-lighten py-2 px-4 shadow-btn uppercase font-extrabold tracking-widest text-btn-text transition-all duration-150 ease-in-out hover:opacity-75 text-center w-full focus:outline-none"
-          type="submit" name="submit" @click="login()">Continue to Store
-      </button>
-    </form>
+    <!--<form>-->
+    <!--  <input-->
+    <!--      class="bg-gray-800 text-white block py-2 px-4 w-full mb-5 border border-light text-center transition-colors duration-150 ease-in-out focus:border-yellow-400 focus:outline-none"-->
+    <!--      autocomplete="off" pattern="^[A-Za-z0-9_]{1,16}$" spellcheck="false" maxlength="16" v-model = "username"/>-->
+    <!--  <button-->
+    <!--      class="bg-btn border border-lighten py-2 px-4 shadow-btn uppercase font-extrabold tracking-widest text-btn-text transition-all duration-150 ease-in-out hover:opacity-75 text-center w-full focus:outline-none"-->
+    <!--      type="submit" name="submit">Continue to Store-->
+    <!--  </button>-->
+    <!--</form>-->
+
+    <div @click="login">test</div>
   </div>
 </template>
 
@@ -32,22 +34,12 @@ async function login() {
   if (username.length == 0) {
     return false;
   }
-  // const {data: result} = await web.get(`https://api.mojang.com/users/profiles/minecraft/${username.value}`)
 
-  axios.get(`https://api.mojang.com/users/profiles/minecraft/${username.value}`).then(
-      response => {
-        console.log(response)
-        //请求成功后更新List的数据
-
-      },
-      error => {
-        //请求后更新List的数据
-
-      }
-  )
+  const {
+    data: result
+  } = await web.get('mojang/users/profiles/minecraft/Ba1oretto')
 
   console.log(result)
-
 }
 </script>
 <style scoped>
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    proxy: {
+      '/mojang': {
+        target: 'https://api.mojang.com/',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/mojang/, '')
+      }
+    }
+  }
 })
