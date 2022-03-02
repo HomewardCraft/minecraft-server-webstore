fc26cf34de9ab3e6ae65b05adbd762901ed99bec
Caishangqi <Caiziii@126.com>
2022-03-02 01:19:17 -0800

    vue2 他真的，我哭死

--- src/components/util/SearchUtil.vue
+++ src/components/util/SearchUtil.vue
@@ -2,12 +2,13 @@
   <div class="category-heading pt-4 text-center">
     <div class="text-5xl text-white font-bold">Extras</div>
     <div class="input-wrap w-full lg:w-1/2 mx-auto relative mt-6">
-      <input ref="inputtext"
-        class="wi w-full bg-gray-900 rounded-md text-white border border-lighten text-lg py-3 px-6 transition-colors duration-200 ease-in-out focus:outline-none focus:border-yellow-400"
-        id="searchTerm">
+      <input ref="inputtext" @focus="focus"
+             @blur="unfocus"
+             class="wi w-full bg-gray-900 rounded-md text-white border border-lighten text-lg py-3 px-6 transition-colors duration-200 ease-in-out focus:outline-none focus:border-yellow-400"
+             id="searchTerm">
       <div
           class="placeholder pointer-events-none absolute top-0 left-0 text-lg text-gray-700 py-2 leading-loose px-6 transition-opacity duration-200 ease-in-out">
-        按 Enter 进行聚焦搜索
+        {{ placeholder }}
       </div>
       <label for="searchTerm" class="icon absolute right-0 top-0 m-px p-4 rounded-md pointer-events-none">
         <svg class="w-5 h-5 text-gray-700" fill="#55575c" viewBox="0 0 20 20">
@@ -29,26 +30,38 @@ export default {
   setup() {
 
     //获取当前实例对象 vue2中的this
-    const { proxy, ctx } = getCurrentInstance()
+    const {proxy, ctx} = getCurrentInstance()
 
-    const inputtext = ref(null)
+    let placeholder = ref('按 Enter 进行聚焦搜索')
 
-    function saveKeys(event){
-      // point.x = event.pageX
-      // point.y = event.pageY
+    function saveKeys(event) {
 
-      if (event.code == "Enter")
+      if (event.code === "Enter")
         ctx.$refs.inputtext.focus()
     }
 
+    function focus() {
+      placeholder.value = " "
+    }
+
+    function unfocus() {
+      placeholder.value = "按 Enter 来聚焦搜索"
+    }
+
     //实现鼠标“打点”相关的生命周期钩子
-    onMounted(()=>{
-      window.addEventListener('keypress',saveKeys)
+    onMounted(() => {
+      window.addEventListener('keypress', saveKeys)
     })
 
-    onBeforeUnmount(()=>{
-      window.removeEventListener('keypress',saveKeys)
+    onBeforeUnmount(() => {
+      window.removeEventListener('keypress', saveKeys)
     })
+
+    return {
+      placeholder,
+      focus,
+      unfocus
+    }
   }
 }
 </script>
@@ -61,6 +74,7 @@ export default {
 .wi {
   background-color: #0d0e0f;
 }
+
 .wi2 {
   background-color: #55575c;
 }
