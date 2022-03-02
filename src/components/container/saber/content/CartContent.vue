ffa661aa12ceab779e294222f148ef2388054e48
Ba1oretto <sunjiamu@outlook.com>
2022-03-02 14:00:04 +0900

    完善侧边栏

---
+++ src/components/container/saber/content/CartContent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="top relative flex items-center justify-between m-6">
+    <div class="title text-white font-bold text-2xl">
+      Your Cart
+      <!-- TODO 购物车一览 -->
+      <small class="text-gray-500">(0)</small>
+    </div>
+    <div class="currency-checkout">
+      <currency-checkout/>
+    </div>
+  </div>
+  <div class="px-6 py-12 text-gray-500 text-center">
+    <p>Looks like your cart is empty! Add some items to get started.</p>
+  </div>
+</template>
+
+<script>
+import CurrencyCheckout from "./CurrencyCheckout.vue";
+export default {
+  name: "CartContent",
+  components: {CurrencyCheckout}
+}
+</script>
---
+++ src/components/container/saber/content/UserContent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex items-center font-bold text-lg bg-custom-1200 pt-5 px-6">
+    <!-- TODO 图片和玩家名字 -->
+    <img src="https://visage.surgeplay.com/bust/64/619377de9ada41388ef93dbf9fe56320"
+         alt="Ba1oretto's Minecraft Skin"/>
+    <div class="ml-5">
+      <div class="label text-xs uppercase tracking-widest leading-none text-gray-500">Shopping As</div>
+      <div class="username">Ba1oretto</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserContent"
+}
+</script>
+
+<script setup>
+
+</script>
