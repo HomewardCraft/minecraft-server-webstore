cb31d84d10b253911e1bd741a2f22d204c99844c
Ba1oretto <sunjiamu@outlook.com>
2022-03-04 07:29:13 +0900

    起草rank

---
+++ src/components/routes/RanksRoute/RankLevel.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="rank bg-gray-800 rank-vip">
+    <div class="heading text-center text-base font-bold py-8 px-4 text-shadow-md" style="background-color: rgb(233, 179, 8);">
+      <div class="tag">
+        <svg class="h-4 mx-auto" fill="white" style="filter:drop-shadow(4px 0px 0 rgba(0,0,0,0.3))" viewBox="0 0 15 5"
+             xmlns="http://www.w3.org/2000/svg">
+          <path d="M0 0H1V3H0V0Z"/>
+          <path d="M2 4H1V3H2V4Z"/>
+          <path d="M3 4V5H2V4H3Z"/>
+          <path d="M4 3V4H3V3H4Z"/>
+          <path d="M4 3V0H5V3H4Z"/>
+          <path d="M6 0H9V1H8V4H9V5H6V4H7V1L6 1V0Z"/>
+          <path d="M10 0H14V1H11V2H14V3H11V5H10V0Z"/>
+          <path d="M14 2H15V1H14V2Z"/>
+        </svg>
+      </div>
+      <div>
+        <div class="leading-0 pt-4">$1.00 - 10.00 USD</div>
+      </div>
+    </div>
+    <div class="content font-bold text-gray-500 p-6">
+      <div class="title text-white text-left font-bold pb-3">Realm</div>
+      <ul class="pb-8">
+        <li class="flex items-center pb-2">
+          <svg class="w-5 h-5 mr-3 opacity-50" fill="currentColor" viewBox="0 0 20 20">
+            <path fill-rule="evenodd" d="M3 4a1 1 0 011-1h4a1 1 0 010 2H6.414l2.293 2.293a1 1 0 11-1.414 1.414L5 6.414V8a1 1 0 01-2 0V4zm9 1a1 1 0 010-2h4a1 1 0 011 1v4a1 1 0 01-2 0V6.414l-2.293 2.293a1 1 0 11-1.414-1.414L13.586 5H12zm-9 7a1 1 0 012 0v1.586l2.293-2.293a1 1 0 111.414 1.414L6.414 15H8a1 1 0 010 2H4a1 1 0 01-1-1v-4zm13-1a1 1 0 011 1v4a1 1 0 01-1 1h-4a1 1 0 010-2h1.586l-2.293-2.293a1 1 0 111.414-1.414L15 13.586V12a1 1 0 011-1z" clip-rule="evenodd"></path>
+          </svg>
+          <span>325x325 Size</span></li>
+        <li class="flex items-center pb-2">
+          <svg class="w-5 h-5 mr-3 opacity-50" fill="currentColor" viewBox="0 0 20 20">
+            <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path>
+          </svg>
+          <span>8 Members</span>
+        </li>
+        <li class="flex items-center">
+          <svg class="w-5 h-5 mr-3 opacity-50" fill="currentColor" viewBox="0 0 20 20">
+            <path d="M10 2a5 5 0 00-5 5v2a2 2 0 00-2 2v5a2 2 0 002 2h10a2 2 0 002-2v-5a2 2 0 00-2-2H7V7a3 3 0 015.905-.75 1 1 0 001.937-.5A5.002 5.002 0 0010 2z"></path>
+          </svg>
+          <span>Not always open</span>
+        </li>
+      </ul>
+      <div class="title text-white text-left font-bold pb-3">Auction House</div>
+      <ul class="pb-6">
+        <li class="flex items-center pb-2">
+          <svg class="w-5 h-5 mr-3 opacity-50" fill="currentColor" viewBox="0 0 20 20">
+            <path d="M10 3.5a1.5 1.5 0 013 0V4a1 1 0 001 1h3a1 1 0 011 1v3a1 1 0 01-1 1h-.5a1.5 1.5 0 000 3h.5a1 1 0 011 1v3a1 1 0 01-1 1h-3a1 1 0 01-1-1v-.5a1.5 1.5 0 00-3 0v.5a1 1 0 01-1 1H6a1 1 0 01-1-1v-3a1 1 0 00-1-1h-.5a1.5 1.5 0 010-3H4a1 1 0 001-1V6a1 1 0 011-1h3a1 1 0 001-1v-.5z"/>
+          </svg>
+          <span>4 Items</span>
+        </li>
+        <li class="flex items-center">
+          <svg class="w-5 h-5 mr-3 opacity-50" fill="currentColor" viewBox="0 0 20 20">
+            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clip-rule="evenodd"/>
+          </svg>
+          <span>8 Hours</span>
+        </li>
+      </ul>
+      <div class="title text-white text-left font-bold pb-3">Miscellaneous</div>
+      <ul>
+        <li class="flex items-center pb-2">
+          <svg class="w-5 h-5 mr-3 opacity-50" fill="currentColor" viewBox="0 0 20 20">
+            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 100-2 1 1 0 000 2zm7-1a1 1 0 11-2 0 1 1 0 012 0zm-.464 5.535a1 1 0 10-1.415-1.414 3 3 0 01-4.242 0 1 1 0 00-1.415 1.414 5 5 0 007.072 0z" clip-rule="evenodd"></path>
+          </svg>
+          <span>Basic Emoji Pack</span>
+        </li>
+        <li class="flex items-center">
+          <svg class="w-5 h-5 mr-3 opacity-50" fill="currentColor" viewBox="0 0 20 20">
+            <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
+          </svg>
+          <span>6 Personal Homes</span>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RankLevel"
+}
+</script>
+
+<script setup>
+
+</script>
--- src/pages/RanksRoute.vue
+++ src/pages/RanksRoute.vue
@@ -1,10 +1,17 @@
 <template>
-
+  <rank-title/>
+  <div class="rank-grid grid md:grid-cols-2 lg:grid-cols-4 gap-4 mt-12">
+    <rank-level/>
+  </div>
 </template>
 
 <script>
+import RankTitle from "../components/routes/RanksRoute/RankTitle.vue";
+import RankLevel from "../components/routes/RanksRoute/RankLevel.vue";
+
 export default {
-  name: "RanksRoute"
+  name: "RanksRoute",
+  components: {RankLevel, RankTitle}
 }
 </script>
 
