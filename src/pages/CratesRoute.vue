e169f8dea0bba8631d2a1456a94fc56c9c38d801
Ba1oretto <sunjiamu@outlook.com>
2022-03-03 18:35:13 +0900

    麻了

--- src/pages/CratesRoute.vue
+++ src/pages/CratesRoute.vue
@@ -7,37 +7,102 @@
 
 <script>
 import CrateShowcase from "../components/routes/CratesRoute/CrateShowcase.vue";
+
 export default {
   name: "CratesRoute",
   components: {CrateShowcase},
-  beforeCreate() {
-    // let http = this.$.appContext.config.globalProperties.$http;
-
-    // getItemList()
-    //
-    // async function getItemList() {
-    //   const {
-    //     data: result
-    //   } = await http.get(`fantang/crate`)
-    //
-    //   console.log(result);
-    // }
-  }
 }
 </script>
 
 <script setup>
-import {getCurrentInstance} from "vue";
+import {getCurrentInstance, ref} from "vue";
+
 let http = getCurrentInstance().appContext.config.globalProperties.$http;
+
+let crates = ref('')
+
+function getItemList() {
+  http.get(`fantang/api/category/crates`).then(
+      res => {
+        let result = res.data
+        let imageAddress = ''
+        let crates = {}
+        let crateArray = []
+
+        for (let i = 0; i < result.data.length; i++) {
+          let item = result.data[i];
+
+
+          if (imageAddress === '') {
+            imageAddress = item.itemBasicInfo.imageAddress
+          }
+
+
+          if (imageAddress === item.itemBasicInfo.imageAddress) {
+            crateArray.push(item)
+          }
+
+          if (imageAddress !== item.itemBasicInfo.imageAddress) {
+            let name = imageAddress.substring(45, imageAddress.length - 10);
+            crates[name] = crateArray
+            crateArray = []
+            imageAddress = item.itemBasicInfo.imageAddress
+            crateArray.push(item)
+          }
+
+          if (i === result.data.length - 1) {
+            let name = imageAddress.substring(45, imageAddress.length - 10);
+            crates[name] = crateArray
+          }
+        }
+
+        console.log('final crateArray: ', crateArray)
+        console.log('final crates: ', crates)
+
+        console.log(result.data)
+        crates.value = crates
+      }
+  )
+
+  // let imageAddress = ''
+  // let crates = {}
+  // let crateArray = []
+  //
+  // for (let i = 0; i < result.data.length; i++) {
+  //   let item = result.data[i];
+  //
+  //
+  //   if (imageAddress === '') {
+  //     imageAddress = item.itemBasicInfo.imageAddress
+  //   }
+  //
+  //
+  //   if (imageAddress === item.itemBasicInfo.imageAddress) {
+  //     crateArray.push(item)
+  //   }
+  //
+  //   if (imageAddress !== item.itemBasicInfo.imageAddress) {
+  //     let name = imageAddress.substring(45, imageAddress.length - 10);
+  //     crates[name] = crateArray
+  //     crateArray = []
+  //     imageAddress = item.itemBasicInfo.imageAddress
+  //     crateArray.push(item)
+  //   }
+  //
+  //   if (i === result.data.length - 1) {
+  //     let name = imageAddress.substring(45, imageAddress.length - 10);
+  //     crates[name] = crateArray
+  //   }
+  // }
+  //
+  // console.log('final crateArray: ', crateArray)
+  // console.log('final crates: ', crates)
+  //
+  // console.log(result.data)
+  // crates.value = crates
+}
 
 getItemList()
 
-async function getItemList() {
-  const {
-    data: result
-  } = await http.get(`fantang/api/category/crates`)
-
-  console.log(result);
-}
-
+console.log(crates)
 </script>
