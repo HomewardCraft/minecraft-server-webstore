40ab7f58c7c9dbaa12b17d202658dc9e7b4db9b7
Ba1oretto <sunjiamu@outlook.com>
2022-03-06 22:06:54 +0900

    嘉然, 我真的好喜欢你啊

--- src/components/container/toast/AddCartMessage.vue
+++ src/components/container/toast/AddCartMessage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="toast-wrap">
-    <div class="toast inline-flex text-white font-bold border border-lighten rounded-lg text-black py-3 px-6 shadow-xl bg-green-700">
+    <div class="toast inline-flex text-white font-bold border border-lighten rounded-lg py-3 px-6 shadow-xl bg-green-700">
       Added {{  }} to your card
     </div>
   </div>
