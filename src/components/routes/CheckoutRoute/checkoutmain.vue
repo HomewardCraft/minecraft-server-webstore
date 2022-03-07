d8edaa17026b190afd2e799c56d477a462e709e8
Ba1oretto <sunjiamu@outlook.com>
2022-03-08 00:21:43 +0900

    修复样式

--- src/components/routes/CheckoutRoute/checkoutmain.vue
+++ src/components/routes/CheckoutRoute/checkoutmain.vue
@@ -23,27 +23,21 @@
             购买前请确保您的库存中有空间。</p></div>
       </div>
     </section>
+
     <section id="details" class="lg:w-2/3 mb-10">
       <div>
-        <div
-            class="font-bold text-white text-xl mb-6 relative flex items-center justify-between cursor-pointer group">
+        <div class="font-bold text-white text-xl mb-6 relative flex items-center justify-between cursor-pointer group">
           <span>邮箱地址</span>
           <svg class="w-4 h-4 opacity-75" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd"
-                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                  clip-rule="evenodd"></path>
+            <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"/>
           </svg>
-          <div
-              class="popup absolute top-0 right-0 mt-8 text-gray-500 bg-black-80 rounded-lg text-sm font-normal max-w-md py-1 px-2 text-right pointer-events-none opacity-0 transition-opacity duration-150 ease-in-out group-hover:opacity-100">
-            您的电子邮件将仅用于发送付款确认和其他重要信息。 你永远都不会
-            由 Arcane Travel 发送垃圾邮件或发送垃圾邮件。
+          <div class="popup absolute top-0 right-0 mt-8 text-gray-500 bg-black-80 rounded-lg text-sm font-normal max-w-md py-1 px-2 text-right pointer-events-none opacity-0 transition-opacity duration-150 ease-in-out group-hover:opacity-100">
+            您的电子邮件将仅用于发送付款确认和其他重要信息。 你永远都不会由 Arcane Travel 发送垃圾邮件或发送垃圾邮件。
           </div>
         </div>
-        <div class="form-row"><label for="email" class="sr-only">Email Address</label>
-          <input name="email" id="email" v-model="Email" ref="email"
-                 type="email" :class="ifisEmail"
-                 class="appearance-none bg-gray-800 border border-lighten font-bold text-lg block w-full py-3 px-5 transition-colors duration-150 ease-in-out focus:border-yellow-400 focus:outline-none focus:border-piston"
-                 placeholder="example@arcanetravel.com">
+        <div class="form-row" @click.once="enableCheck">
+          <label for="email" class="sr-only">邮箱地址</label>
+          <input name="email" id="email" v-model="Email" ref="email" type="email" :class="ifIsEmail" placeholder="example@arcanetravel.com" class="appearance-none bg-gray-800 border border-lighten font-bold text-lg block w-full py-3 px-5 transition-colors duration-150 ease-in-out focus:border-yellow-400 focus:outline-none focus:border-piston">
         </div>
       </div>
     </section>
@@ -78,7 +72,9 @@ let username = ref(GLOBAL_DATA.state.user.ign)
 let uuid = ref(GLOBAL_DATA.state.user.uuid)
 let url = ref('https://visage.surgeplay.com/bust/128/')
 //样式控制
-let ifisEmail = ref('invalid')
+let ifIsEmail = ref('')
+
+let isCheck = false
 
 function delayRef(value, delay) {
   let timer
@@ -99,41 +95,49 @@ function delayRef(value, delay) {
   })
 }
 
-let Email = delayRef(GLOBAL_DATA.state.fields.email_add, 1000)
+let Email = ref('')
 
 function logOut() {
   router.push('/')
   GLOBAL_DATA.commit('logOut', 'logout')
 }
 
-function check(value) {
-  var reg = new RegExp("^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$"); //正则表达式
+function enableCheck() {
+  isCheck = true
+  check(Email.value)
+}
 
+function check(value) {
+  if (!isCheck) {
+    return
+  }
+
+  let reg = new RegExp("^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$"); //正则表达式
   if (value === "") { //输入不能为空
-    console.log("(!) 错误格式")
-    ifisEmail.value = 'invalid'
+    // console.log("(!) 错误格式")
+    ifIsEmail.value = 'invalid'
     return false;
   } else if (!reg.test(value)) { //正则验证不通过，格式不对
-    console.log("(!) 错误格式")
-    ifisEmail.value = 'invalid'
+    // console.log("(!) 错误格式")
+    ifIsEmail.value = 'invalid'
     return false;
   } else {
-    console.log("(+) 通过")
+    // console.log("(+) 通过")
     GLOBAL_DATA.state.fields.email = true
-    ifisEmail.value = ' '
+    GLOBAL_DATA.state.fields.email_add = value
+    console.log(GLOBAL_DATA.state.fields.email_add)
+    ifIsEmail.value = ''
     return true;
   }
 }
 
 watch(() => GLOBAL_DATA.state.fields, (newValue, oldValue) => {
   console.log("fields更新了")
-
 }, {deep: true})
 
 watch(Email, (newValue, oldValue) => {
-  console.log('Email', newValue, oldValue)
   check(Email.value)
-}, {immediate: true})
+})
 
 </script>
 
