e7a8df7250c1e2c8024e05eccf65bba5d7342be2
Ba1oretto <sunjiamu@outlook.com>
2022-03-08 04:39:29 +0900

    ripi

---
+++ src/components/container/saber/MainContent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="content-user">
+    <button-content/>
+  </div>
+</template>
+
+<script>
+import ButtonContent from "./content/ButtonContent.vue";
+export default {
+  name: "MainContent",
+  components: {ButtonContent}
+}
+</script>
