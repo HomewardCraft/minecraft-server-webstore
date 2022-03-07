dec441fd039ccc212ea86c0de8f248908aa4592c
Caishangqi <Caiziii@126.com>
2022-03-07 04:29:35 -0800

    优先选择微信支付

--- src/components/routes/CheckoutRoute/checkoutmain.vue
+++ src/components/routes/CheckoutRoute/checkoutmain.vue
@@ -50,12 +50,14 @@
     <section id="method" class="lg:w-4/5"><h4 class="font-bold text-white text-xl mb-6">支付方式</h4>
       <div class="gateways"><label
           class="gateway flex items-center relative border border-lighten py-4 px-6 mb-4 transition-opacity duration-150 ease-in-out cursor-pointer opacity-75 opacity-100"
-          data-gateway="PayPal"><input type="radio" id="gateway-600346" name="gateway" class="absolute opacity-0"
-                                       value="600346"><span
+          data-gateway="PayPal">
+        <input type="radio" id="gateway-600346" name="gateway" class="absolute opacity-0"
+               value="600346"><span
           class="dot block rounded-full border-2 border-lighten mr-4 opacity-25 transition-opacity duration-150 ease-in-out"><span
-          class="inner block m-1 h-2 w-2 bg-white rounded-full"></span></span><span class="logo block"><img
-          src="https://cdn.tebex.io/store/845008/templates/75753/assets/db15f53773b3bcf37891eb0d011b239df91790bc.png"
-          alt="PayPal"></span><span class="cards block"></span></label></div>
+          class="inner block m-1 h-2 w-2 bg-white rounded-full"></span></span><span class="logo block">
+        <img
+            src="src/assets/images/payment/wechat.png"
+            alt="Wechat"></span><span class="cards block"></span></label></div>
     </section>
   </div>
 </template>
@@ -92,6 +94,7 @@ function delayRef(value, delay) {
     }
   })
 }
+
 let Email = delayRef(GLOBAL_DATA.state.fields.email_add, 1000)
 
 function logOut() {
@@ -141,14 +144,9 @@ export default {
   background: linear-gradient(to top, #181a1b, rgba(24, 26, 27, 0));
 }
 
-.invalid{
+.invalid {
   --tw-bg-opacity: 1;
   border-color: rgb(239 68 68 / var(--tw-bg-opacity));
 }
 
-
-
-.valid{
-
-}
 </style>
