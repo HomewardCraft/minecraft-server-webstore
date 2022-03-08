80ed0e944695f6ebff2d88fd5831d228374a2cc5
Caishangqi <2470159730@qq.com>
2022-03-08 13:05:34 -0800

    pannle基础静态面板完成

--- src/components/ComponentsLogin/LoginHelp.vue
+++ src/components/ComponentsLogin/LoginHelp.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="lg:col-span-3 p-8 bg-gray-800">
-    <div class="font-bold text-2xl mb-4 text-gray-50/80">如何知道我的用户名?</div>
-    <p class="text-gray-500 text-lg mb-3">你的用户名应该是我们经过授权的用户名，并且该用户名应该有专门的密码。如果你是管理人员并且忘记了自己的账号或者密码，请联系我们或者自行查找数据库。若你不是管理人员则你无法通过你的游戏账号登录该面板，感谢您的谅解</p>
+    <div class="font-bold text-xl mb-4 text-gray-50/80">如何知道我的用户名?</div>
+    <p class="text-gray-500 text-sm mb-3">你的用户名应该是我们经过授权的用户名，并且该用户名应该有专门的密码。如果你是管理人员并且忘记了自己的账号或者密码，请联系我们或者自行查找数据库。若你不是管理人员则你无法通过你的游戏账号登录该面板，感谢您的谅解</p>
     <img src="src/assets/images/database.png" alt="suggestion"/>
   </div>
 </template>
