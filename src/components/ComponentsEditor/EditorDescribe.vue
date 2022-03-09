9d6366eabef506aa37cfa271c7172ded88ae7c08
Caishangqi <2470159730@qq.com>
2022-03-09 00:27:35 -0800

    初步实现

--- src/components/ComponentsEditor/EditorDescribe.vue
+++ src/components/ComponentsEditor/EditorDescribe.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="lg:col-span-3 p-8 bg-gray-800">
     <div class="font-bold text-xl mb-4 text-gray-50/80">如何知道我的用户名?</div>
-    <md-editor v-model="text"/>
+    <editorrender/>
   </div>
 </template>
 
@@ -14,6 +14,7 @@ export default {
 <script setup>
 
 
+import Editorrender from "./edit/defineComponent.vue";
 </script>
 
 <style scoped>
---
+++ src/components/ComponentsEditor/edit/defineComponent.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="codeShare">
+    <div>
+      <!-- 只读模式 :previewOnly="true" -->
+      <!-- :sanitize="sanitize" 使用会导致代码不高亮 -->
+      <MdEditor
+          toolbarsExclude="['link', 'mermaid', 'katex', 'github']"
+          v-model="text"
+          @onSave="codeSave"
+      >
+      </MdEditor>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent, reactive, toRefs, onMounted} from 'vue'
+
+import MdEditor from 'md-editor-v3';
+import 'md-editor-v3/lib/style.css';
+// 使用 sanitizeHtml 处理不安全的 html
+import sanitizeHtml from 'sanitize-html';
+
+interface shareData {
+  text: string,
+  load: number,
+  isDis: boolean
+}
+
+export default defineComponent({
+  name: "editorrender",
+  components: {MdEditor},
+  setup() {
+    const data = <shareData>reactive({
+      text: '',
+      load: 0,
+      isDis: false
+    })
+
+    const codeSave = (v: string): void => {
+      localStorage.setItem('codeSave', v)
+    }
+
+    const sanitize = (html: string): string => {
+      console.log(sanitizeHtml(html))
+      return sanitizeHtml(html)
+    }
+
+    const href = window.location.href;
+    const url = href.substring(0, href.length - 10);
+
+
+    onMounted(() => {
+      if (localStorage.getItem('codeSave')) {
+        data.text = localStorage.getItem('codeSave') || ''
+      }
+    })
+
+    return {
+      ...toRefs(data),
+      codeSave,
+      sanitize,
+    }
+  },
+});
+</script>
+
+<style scoped>
+/deep/ .md {
+  height: 600px !important;
+}
+</style>
