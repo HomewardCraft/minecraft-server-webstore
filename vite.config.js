8298803a634735d97ed1d2dc53f51a34a0e1af8e
Ba1oretto <sunjiamu@outlook.com>
2022-03-12 06:15:07 +0900

    乐色elui

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,22 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    proxy: {
-      '/fantang': {
-        target: 'https://fantang.cc/',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/fantang/, '')
-      },
+    plugins: [vue()],
+    server: {
+        proxy: {
+            '/fantang': {
+                target: 'https://fantang.cc/',
+                changeOrigin: true,
+                rewrite: path => path.replace(/^\/fantang/, '')
+            },
 
-      '/local': {
-        target: 'http://127.0.0.1:50012/',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/local/, '')
-      }
+            '/local': {
+                target: 'http://127.0.0.1:50012/',
+                changeOrigin: true,
+                rewrite: path => path.replace(/^\/local/, '')
+            }
+        }
     }
-  }
 })
