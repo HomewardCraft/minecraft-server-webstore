b3aa61f265783578ad81da94bf887f64adfc7356
Caishangqi <2470159730@qq.com>
2022-03-23 00:04:22 -0700

    重载指令

--- src/main/java/com/arcanetravel/command/Main.java
+++ src/main/java/com/arcanetravel/command/Main.java
@@ -1,13 +1,16 @@
 package com.arcanetravel.command;
 
+import com.arcanetravel.shopconnectbridge;
 import me.mattstudios.mf.annotations.Command;
 import me.mattstudios.mf.annotations.Default;
 import me.mattstudios.mf.annotations.SubCommand;
 import me.mattstudios.mf.base.CommandBase;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
 @Command("webstore")
 public class Main extends CommandBase {
+    shopconnectbridge plugin = shopconnectbridge.plugin;
 
     @Default
     public void defaultCommand(final CommandSender commandSender) {
@@ -17,7 +20,9 @@ public class Main extends CommandBase {
 
     @SubCommand("reload")
     public void testSubCommand(final CommandSender commandSender) {
-        commandSender.sendMessage("Test!");
+        commandSender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7webstore reload success!"));
+        plugin.reloadConfig();
+        plugin.saveConfig();
     }
 
 
