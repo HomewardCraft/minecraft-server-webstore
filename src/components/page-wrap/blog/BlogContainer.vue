297b96c19d28c7505b84b4740d139d5bbbe25803
Ba1oretto <sunjiamu@outlook.com>
2022-04-04 08:43:34 +0900

    header写好了

--- src/components/page-wrap/blog/BlogContainer.vue
+++ src/components/page-wrap/blog/BlogContainer.vue
@@ -84,11 +84,12 @@ const textSubstring = (text) => {
 }
 
 const setColor = (type) => {
-  console.log(type)
-  switch (type) {
-
+  switch (type.toString().toLowerCase()) {
+    case 'update': return 'color: rgb(245, 183, 43)'
+    case 'dev blog': return 'color: rgb(51, 241, 127)'
+    case 'event': return 'color: rgb(67, 204, 218)'
+    case 'misc': return 'color: rgb(255, 134, 218)'
   }
-  return 'color: rgb(245, 183, 43)'
 }
 
 
