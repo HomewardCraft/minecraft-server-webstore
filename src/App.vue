e4b89e4f6dd2cab8cf51adb8157b64a058695d4f
Ba1oretto <sunjiamu@outlook.com>
2022-04-04 01:24:06 +0900

    1

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script setup>
 import PageFooter from "./components/PageFooter.vue";
+import PageHeader from "./components/PageHeader.vue";
 </script>
 
 <template>
   <div id="_frame">
     <div class="body-wrap grid grid-rows-body h-full">
+      <page-header/>
+      <router-view/>
       <page-footer/>
     </div>
   </div>
 </template>
-
-<style>
-</style>
