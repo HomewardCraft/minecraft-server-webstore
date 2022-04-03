2051cd69f7950a124c0d316c4bf4e0489b8d002a
Caishangqi <2470159730@qq.com>
2022-04-02 21:59:04 -0700

    Change

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import MainPage from "./components/MainPage.vue";
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <main-page></main-page>
 </template>
 
 <style>
