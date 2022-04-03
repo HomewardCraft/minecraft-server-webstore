08c5a1f392ba0d8e34b9edda6a9a4f2880c46218
Ba1oretto <sunjiamu@outlook.com>
2022-04-04 07:48:28 +0900

    同步出来

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup>
-import PageFooter from "./components/PageFooter.vue";
 import PageWrap from "./components/PageWrap.vue";
 </script>
 
 <template>
   <div id="_frame">
     <div class="body-wrap grid grid-rows-body h-full">
+      <router-view name="header"/>
       <page-wrap/>
+      <router-view name="footer"/>
     </div>
   </div>
 </template>
--- src/components/PageWrap.vue
+++ src/components/PageWrap.vue
@@ -1,20 +1,14 @@
 <template>
-  <router-view name="header"/>
   <div class="page-wrap">
     <router-view/>
   </div>
-  <router-view name="footer"/>
 </template>
 
 <script>
-import PageHeader from "./PageHeader.vue";
-
 export default {
   name: "PageWrap",
-  components: {PageHeader}
 }
 </script>
 
 <script setup>
-import PageFooter from "./PageFooter.vue";
 </script>
